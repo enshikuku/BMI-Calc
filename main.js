0650034fec043cc63c59b00383dbeeb7470db35f
Shii_039 <enshikuku39@gmail.com>
2022-07-17 14:03:33 +0300

    updates

---
+++ main.js
@@ -0,0 +1,75 @@
+let heightInput = document.querySelector('.heightInput') 
+let weightInput = document.querySelector('.weightInput') 
+let calculateButton = document.querySelector('.calculate')
+let heightFeet = document.querySelector('.heightInput-1')
+let heightInches = document.querySelector('.heightInput-1-1') 
+let weightImperial = document.querySelector('.weightInput-1') 
+let calculateBtn = document.querySelector('.calculate-1') 
+let result = document.querySelector('.result')
+let metricbtn = document.querySelector('.metric')
+let imperialbtn = document.querySelector('.imperial')
+let metCalc = document.querySelector('.metric-calculator')
+let impCalc = document.querySelector('.imperial-calculator')
+let statement1 = document.querySelector('#result-statement-1')
+let statement2 = document.querySelector('#result-statement-2')
+let statement3 = document.querySelector('#result-statement-3')
+let statement4 = document.querySelector('#result-statement-4')
+let BMI, height, weight 
+
+calculateButton.addEventListener('click', ()=>{
+
+    height = heightInput.value/100 
+    weight = weightInput.value 
+    BMI = weight/(height**2)
+    result.innerText = BMI.toFixed(1)
+    
+    statement1.style.backgroundColor = 'white'
+    statement2.style.backgroundColor = 'white'
+    statement3.style.backgroundColor = 'white'
+    statement4.style.backgroundColor = 'white'
+    if(BMI < 18.5){
+        statement1.style.backgroundColor = 'lightgreen'     
+    } else if ((BMI >= 18.5) && (BMI <= 24.9)){
+        statement2.style.backgroundColor = 'lightgreen' 
+    } else if ((BMI > 25) && (BMI <= 29.9 )){
+        statement3.style.backgroundColor = 'lightgreen' 
+    } else if (BMI > 29.9){
+        statement4.style.backgroundColor = 'lightgreen'
+    }
+}) 
+calculateBtn.addEventListener('click', ()=>{
+    let impHeight = (heightFeet.value * 30.48) + (heightInches.value * 2.54)
+    height = impHeight / 100
+    weight = weightImperial.value * 0.453592
+    BMI = weight/(height**2)
+    result.innerText = height
+    
+    
+    statement1.style.backgroundColor = 'white'
+    statement2.style.backgroundColor = 'white'
+    statement3.style.backgroundColor = 'white'
+    statement4.style.backgroundColor = 'white'
+    if(BMI < 18.5){
+        statement1.style.backgroundColor = 'lightgreen'     
+    } else if ((BMI >= 18.5) && (BMI <= 24.9)){
+        statement2.style.backgroundColor = 'lightgreen' 
+    } else if ((BMI > 25) && (BMI <= 29.9 )){
+        statement3.style.backgroundColor = 'lightgreen' 
+    } else if (BMI > 29.9){
+        statement4.style.backgroundColor = 'lightgreen'
+    }
+})
+metricbtn.style.backgroundColor = 'lightgreen'
+imperialbtn.style.backgroundColor = 'rgb(211, 210, 210)'
+metricbtn.addEventListener('click', ()=>{
+    metricbtn.style.backgroundColor = 'lightgreen'
+    imperialbtn.style.backgroundColor = 'rgb(211, 210, 210)'
+    metCalc.style.display = 'block'
+    impCalc.style.display = 'none'
+})
+imperialbtn.addEventListener('click', ()=>{
+    metricbtn.style.backgroundColor = 'rgb(211, 210, 210)'
+    imperialbtn.style.backgroundColor = 'lightgreen'
+    metCalc.style.display = 'none'
+    impCalc.style.display = 'block'
+})
